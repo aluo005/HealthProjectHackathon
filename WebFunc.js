2a21409ed3c146af10a6a01666173508bd0486ac
neon-rbjoy <65199159+neon-rbjoy@users.noreply.github.com>
2021-01-17 18:04:56 -0800

    major overhaul of codebase

--- WebFunc.js
+++ WebFunc.js
@@ -1,1 +1,28 @@
-console.log("hi")
+var currentQuestion;
+console.log("basicSyntaxeExucuted");
+
+
+function getInputValue() {
+    console.log("functionStart")
+    inputVal = document.getElementById("group").value;
+    console.log(inputVal);
+    if (inputVal == "yoga") {
+        console.log("yog");
+
+    }
+
+    else if (inputVal == "cardio") {
+        console.log("card");
+    }
+    
+    else if (inputVal == "muscular") {
+        console.log("muscu")
+    }
+
+    else {
+        console.log("else");
+        var temp = document.getElementById("question");
+        temp.textContent = "ERROR: We were unable to figure out what you meant, make sure you follow the instructions about how to input your info.";
+        setTimeout(function () {temp.textContent = "Question 1: Would you like to do cardio, muscular, yoga? (Hint: Please answer with with only the type of excercise you wish to do, written exactly how it is here."}, 3000);
+    }
+}
